b5673f15ddb03b63326625b00afa10984bc91dee
jarno-vos <78414123+jarno-vos@users.noreply.github.com>
2023-11-24 08:56:50 +0100

    no revoke necessary

--- manage-consent-button.php
+++ manage-consent-button.php
@@ -1,6 +1,5 @@
 <?php
-
-defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
+defined( 'ABSPATH' ) or die( "you do not have access to this page!" );
 
 /**
  * 1. Make sure you use the Font-Awesome 5 Library. A free plugin with the same name is available for download.
@@ -9,41 +8,48 @@ defined( 'ABSPATH' ) or die( "you do not have acces to this page!" );
  */
 
 function myCustomManageConsent() {
-	?>
-	<div id="manageconsent" class="cmplz-show-banner"><i class="cmplz-fas fas fa-cookie-bite"></i></div>
-	<style>
-        #manageconsent {
-            font-family: "Font Awesome Free 5";
-            position: fixed;
-            cursor: pointer;
-            width: 60px;
-            height: 60px;
-            bottom: 40px;
-            left: 40px;
-            font-size: 30px;
-            background-color: #29b6f6;
-            color: #fff;
-            line-height: 2;
-            border-radius: 50px;
-            text-align: center;
-            box-shadow: 2px 2px 3px #999;
-        }
+?>
+<div id="my_manage_consent" class="cmplz-show-banner"><i class="cmplz-fas fas fa-cookie-bite"></i></div>
+<style>
+	#my_manage_consent {
+		font-family: "Font Awesome Free 5";
+		position: fixed;
+		cursor: pointer;
+		width: 60px;
+		height: 60px;
+		bottom: 40px;
+		left: 40px;
+		font-size: 30px;
+		background-color: #29b6f6;
+		color: #fff;
+		line-height: 2;
+		border-radius: 50px;
+		text-align: center;
+		box-shadow: 2px 2px 3px #999;
+	}
 
-        #manageconsent:hover {
-            background-color: #333;
-            color: #fff;
-        }
-	    .cmplz-manage-consent {display:none;}
-	</style>
-	<script>
-        document.addEventListener('click', '.cmplz-show-banner', function(){
-            document.querySelectorAll('.cmplz-manage-consent').forEach(obj => {
-                obj.click();
-            });
-        });
-	</script>
+	#my_manage_consent:hover {
+		background-color: #333;
+		color: #fff;
+	}
+	.cmplz-manage-consent {display:none;}
+</style>
+<script>
+	function addEvent(event, selector, callback, context) {
+		document.addEventListener(event, e => {
+			if ( e.target.closest(selector) ) {
+				callback(e);
+			}
+		});
+	}
+	addEvent('click', '.cmplz-show-banner', function(){
+		document.querySelectorAll('.cmplz-manage-consent').forEach(obj => {
+			obj.click();
+		});
+	});
+</script>
 
-	<?php
+<?php
 
 }
 
